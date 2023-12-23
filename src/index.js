fa3007e81edd48ca727712a1240d0cffea8d4b91
BeDanQUynh <ok7600480@gmail.com>
2023-12-23 23:54:03 +0700

    File BTL mới

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ app.engine(
   })
 );
 app.set("view engine", "hbs");
-app.set("views", "./resources/views");
+app.set("views", "./resources/views/");
+
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -31,11 +32,68 @@ app.get('/info2', (req, res) => {
   res.render('info2');
 });
 
+app.get('/info3', (req, res) => {
+  res.render('info3');
+});
+
+app.get('/info4', (req, res) => {
+  res.render('info4');
+});
+
+app.get('/info5', (req, res) => {
+  res.render('info5');
+});
+
+app.get('/info6', (req, res) => {
+  res.render('info6');
+});
+
+app.get('/info7', (req, res) => {
+  res.render('info7');
+});
+
+app.get('/info8', (req, res) => {
+  res.render('info8');
+});
+
+app.get('/info9', (req, res) => {
+  res.render('info9');
+});
+
+app.get('/info10', (req, res) => {
+  res.render('info10');
+});
+
+app.get('/info11', (req, res) => {
+  res.render('info11');
+});
+
+app.get('/info12', (req, res) => {
+  res.render('info12');
+});
+
+app.get('/info13', (req, res) => {
+  res.render('info13');
+});
+
+app.get('/info14', (req, res) => {
+  res.render('info14');
+});
+
+app.get('/info15', (req, res) => {
+  res.render('info14');
+});
+
+app.get('/info16', (req, res) => {
+  res.render('info14');
+});
+
 app.get('/cart', (req, res) => {
   res.render('carts');
 });
 
-app.set("views" , path.join(__dirname,'resources', 'views'))
+app.set("views", path.join(__dirname, 'resources', 'views'))
+
 
 // app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
